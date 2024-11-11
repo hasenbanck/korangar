ebd7e4e70497b93b7ea0964ef1025870f13dece3
Nils Hasenbanck <nils@hasenbanck.de>
2024-11-11 18:28:05 +0100

    WIP

--- korangar/src/world/entity2/mod.rs
+++ korangar/src/world/entity2/mod.rs
@@ -1,4 +1,3 @@
-use std::num::NonZeroU64;
 use std::sync::Arc;
 
 use cgmath::{Point3, Vector2};
@@ -18,6 +17,12 @@ struct GameState {
 }
 
 impl GameState {
+    pub fn clear(&mut self) {
+        self.world.clear();
+        self.entity_mapping.clear();
+        self.player_entity = None;
+    }
+
     pub fn spawn_player(
         &mut self,
         sprite_loader: &mut SpriteLoader,
@@ -30,6 +35,12 @@ impl GameState {
         player_position: WorldPosition,
         client_tick: ClientTick,
     ) {
+        if let Some(player_entity) = self.player_entity {
+            let identifier = self.world.query_one_mut::<&EntityIdentifier>(player_entity).unwrap();
+            self.entity_mapping.remove(&identifier.id);
+            let _ = self.world.despawn(player_entity);
+        }
+
         let player = PlayerBundle::new(
             sprite_loader,
             action_loader,
@@ -61,24 +72,29 @@ impl GameState {
     ) {
         let entity_id = entity_data.entity_id;
 
-        let player = NpcBundle::new(
+        if let Some(entity) = self.entity_mapping.get(&entity_id).copied() {
+            self.entity_mapping.remove(&entity_id);
+            let _ = self.world.despawn(entity);
+        }
+
+        let npc = NpcBundle::new(
             sprite_loader,
             action_loader,
             animation_loader,
             script_loader,
             map,
-            entity_data,
+            &entity_data,
             client_tick,
         );
 
-        let entity = self.world.spawn(player);
+        let entity = self.world.spawn(npc);
         self.entity_mapping.insert(entity_id, entity);
     }
 }
 
 struct EntityIdentifier {
     id: EntityId,
-    entity_type: EntityType, // TODO: NHA Do we need this if we have the marker?
+    entity_type: EntityType,
     job_id: usize,
     sex: Sex,
 }
@@ -88,10 +104,16 @@ struct Position {
     world: Point3<f32>,
 }
 
+struct MovementSpeed(usize);
+
+struct MovementDestination {
+    from: Vector2<usize>,
+    to: Vector2<usize>,
+}
+
 struct Movement {
     steps: Vec<(Vector2<usize>, u32)>,
     starting_timestamp: u32,
-    speed: usize,
     #[cfg(feature = "debug")]
     pathing_vertex_buffer: Option<Arc<Buffer<ModelVertex>>>,
 }
@@ -127,15 +149,15 @@ struct LoadingState {
     animation_loaded: bool,
 }
 
-// TODO: Helper structures for easier spawning.
-
 struct CommonEntityBundle {
     identifier: EntityIdentifier,
     position: Position,
     sprite: Sprite,
     health: Health,
-    details: Details,
     movement: Option<Movement>,
+    movement_destination: Option<MovementDestination>,
+    movement_speed: MovementSpeed,
+    details: Details,
     loading: LoadingState,
 }
 
@@ -146,13 +168,22 @@ impl CommonEntityBundle {
         animation_loader: &mut AnimationLoader,
         script_loader: &ScriptLoader,
         map: &Map,
-        entity_data: EntityData,
+        entity_data: &EntityData,
         client_tick: ClientTick,
     ) -> Self {
-        let grid_position = Vector2::new(entity_data.position.x, entity_data.position.y);
-        let world_position = map.get_world_position(grid_position);
+        let id = entity_data.entity_id;
+        let job_id = entity_data.job as usize;
+        let grid_position = entity_data.position;
+        let grid_position = Vector2::new(grid_position.x, grid_position.y);
+        let get_world_position = map.get_world_position(grid_position);
+        let head_direction = entity_data.head_direction;
 
-        let entity_type = match entity_data.job {
+        let movement_speed = entity_data.movement_speed as usize;
+        let health_points = entity_data.health_points as usize;
+        let maximum_health_points = entity_data.maximum_health_points as usize;
+        let sex = entity_data.sex;
+
+        let entity_type = match job_id {
             45 => EntityType::Warp,
             111 => EntityType::Hidden, // TODO: check that this is correct
             // 111 | 139 => None,
@@ -162,42 +193,57 @@ impl CommonEntityBundle {
             _ => EntityType::Npc,
         };
 
+        let entity_part_files = get_entity_part_files(script_loader, entity_type, job_id, sex);
+        let animation_data = animation_loader
+            .get(sprite_loader, action_loader, entity_type, &entity_part_files)
+            .unwrap();
+        let details_state = ResourceState::Unavailable;
+        let animation_state = AnimationState::new(client_tick);
+
+        // TODO Write a system that resolves the destination!
+        // common.move_from_to(map, position_from, position_to,
+        // client_tick);
+
+        let movement = None;
+        let movement_destination = match entity_data.destination {
+            None => None,
+            Some(destination) => {
+                let from = Vector2::new(entity_data.position.x, entity_data.position.y);
+                let to = Vector2::new(destination.x, destination.y);
+                Some(MovementDestination { from, to })
+            }
+        };
+        let movement_speed = MovementSpeed(movement_speed);
+
         Self {
             identifier: EntityIdentifier {
-                id: entity_data.entity_id,
+                id,
                 entity_type,
-                job_id: entity_data.job as usize,
-                sex: entity_data.sex,
+                job_id,
+                sex,
             },
             position: Position {
                 grid: grid_position,
-                world: world_position,
+                world: get_world_position,
             },
             sprite: Sprite {
-                animation_data: animation_loader
-                    .get(
-                        sprite_loader,
-                        action_loader,
-                        entity_type,
-                        &get_entity_part_files(script_loader, entity_type, entity_data.job as usize, entity_data.sex),
-                    )
-                    .unwrap(),
-                animation_state: AnimationState::new(client_tick),
-                head_direction: entity_data.head_direction,
+                animation_data,
+                animation_state,
+                head_direction,
             },
             health: Health {
-                current: entity_data.health_points as usize,
-                maximum: entity_data.maximum_health_points as usize,
+                current: health_points,
+                maximum: maximum_health_points,
             },
-            details: Details {
-                state: ResourceState::Unavailable,
-            },
+            details: Details { state: details_state },
+            movement,
+            movement_destination,
+            movement_speed,
             loading: LoadingState {
                 request_id: None,
                 sprite_loaded: false,
                 animation_loaded: false,
             },
-            movement: None,
         }
     }
 }
@@ -208,8 +254,10 @@ struct PlayerBundle {
     position: Position,
     sprite: Sprite,
     health: Health,
-    details: Details,
     movement: Option<Movement>,
+    movement_destination: Option<MovementDestination>,
+    movement_speed: MovementSpeed,
+    details: Details,
     loading: LoadingState,
     activity_points: ActivityPoints,
     spell_points: SpellPoints,
@@ -241,8 +289,10 @@ impl PlayerBundle {
             position,
             sprite,
             health,
-            details,
             movement,
+            movement_destination,
+            movement_speed,
+            details,
             loading,
         } = CommonEntityBundle::new(
             sprite_loader,
@@ -250,7 +300,7 @@ impl PlayerBundle {
             animation_loader,
             script_loader,
             map,
-            entity_data,
+            &entity_data,
             client_tick,
         );
 
@@ -259,9 +309,11 @@ impl PlayerBundle {
             position,
             sprite,
             health,
+            movement,
+            movement_destination,
+            movement_speed,
             details,
             loading,
-            movement,
             activity_points,
             spell_points,
         }
@@ -274,9 +326,11 @@ struct NpcBundle {
     position: Position,
     sprite: Sprite,
     health: Health,
+    movement: Option<Movement>,
+    movement_destination: Option<MovementDestination>,
+    movement_speed: MovementSpeed,
     details: Details,
     loading: LoadingState,
-    movement: Option<Movement>,
 }
 
 impl NpcBundle {
@@ -286,7 +340,7 @@ impl NpcBundle {
         animation_loader: &mut AnimationLoader,
         script_loader: &ScriptLoader,
         map: &Map,
-        entity_data: EntityData,
+        entity_data: &EntityData,
         client_tick: ClientTick,
     ) -> Self {
         let CommonEntityBundle {
@@ -294,8 +348,10 @@ impl NpcBundle {
             position,
             sprite,
             health,
-            details,
             movement,
+            movement_destination,
+            movement_speed,
+            details,
             loading,
         } = CommonEntityBundle::new(
             sprite_loader,
@@ -312,8 +368,10 @@ impl NpcBundle {
             position,
             sprite,
             health,
-            details,
             movement,
+            movement_destination,
+            movement_speed,
+            details,
             loading,
         }
     }
